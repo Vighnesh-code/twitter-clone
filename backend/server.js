c25b5daa968c07ace0318ad8dd8b3923b1c2750f
Vighnesh-Code <vighneshzagade4@gmail.com>
2025-04-14 10:31:44 +0000

    added user routes & controllers

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
 import { connectDB } from "./config/db.js";
 import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 app.use(express.json()); // to parse request body
 app.use(express.urlencoded({ extended: true })); // to parse form data (urlencoded)
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(port, () => {
   connectDB();
